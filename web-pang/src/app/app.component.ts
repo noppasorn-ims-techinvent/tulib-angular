7b68f54f296d88c27a50c204ae4404d45a8ef82a
noppasorn-ims-techinvent <noppasorn.ims@techinvent.in.th>
2024-05-29 15:22:18 +0700

    config new web angular

--- web-pang/src/app/app.component.ts
+++ web-pang/src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tulib-angular';
+  title = 'web-pang';
 }
